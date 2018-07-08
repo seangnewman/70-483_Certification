9ce7ebd509ef520a0deb587ae265838613f27df5
Sean Newman <seangnewman@msn.com>
2018-07-07 22:39:01 -0700

    Study of threads

---
+++ 1_2_Background Thread/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace _1_2_Background_Thread
+{
+
+    
+    public static class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProd : {0}", i);
+                Thread.Sleep(1000);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            // If set to false, the  UI thread waits for completion of background task
+            //If set to true, the UI thread completes and ends program
+            t.IsBackground = true;    
+            t.Start();
+
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ 1_3_ParameterizedThreadDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace _1_3_ParameterizedThreadDemo
+{
+   public static  class Program
+    {
+        public static void ThreadMethod(object o)
+        {
+            // cast the object o to integer
+            for(int i = 0; i < (int)o; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);  // Signals that thread has completed
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ParameterizedThreadStart(ThreadMethod));
+            t.Start(5);   // Pass the object (in this case an integer) to thread
+            t.Join();       // UI thread waits for other thread(s)
+        }
+    }
+}
