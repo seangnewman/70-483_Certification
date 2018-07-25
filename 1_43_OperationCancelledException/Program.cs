489d7e9b7ae3f2d8e2a7f39694d0f40a1f1cec32
Sean Newman <seangnewman@msn.com>
2018-07-25 10:27:27 -0700

    finished objective 2

---
+++ 1_43_OperationCancelledException/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _1_43_OperationCancelledException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancellationTokenSource.Token;
+
+            Task task = Task.Run(() => {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.WriteLine("*");
+                    Thread.Sleep(1000);
+                }
+
+                token.ThrowIfCancellationRequested();
+            }, token);
+
+            try
+            {
+                Console.WriteLine("Press enter to stop the task");
+                Console.ReadLine();
+
+                cancellationTokenSource.Cancel();
+                task.Wait();
+
+            }catch(AggregateException e)
+            {
+                Console.WriteLine(e.InnerExceptions[0].Message);
+            }
+
+            
+
+            Console.WriteLine("Press enter to end the application");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 1_44_Cancelled_Continuation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _1_44_Cancelled_Continuation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancellationTokenSource.Token;
+
+            Task task = Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.WriteLine("*");
+                    Thread.Sleep(1000);
+                }
+
+                throw new OperationCanceledException();
+            }, token).ContinueWith((t) =>
+            {
+                t.Exception.Handle((e) =>
+                {
+                    return true;
+                });
+                Console.WriteLine("You have cancelled the task");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+
+          
+
+
+            Console.WriteLine("Press enter to end the application");
+            Console.ReadLine();
+
+
+        }
+    }
+}
